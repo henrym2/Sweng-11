d9835805d9636b6e8fdca4e2fc26b5afb0fab011
johnnywhite612 <johnnywhite612@gmail.com>
2020-02-12 14:50:23 +0000

    Basic skeleton for the submission system.

---
+++ frontend/src/components/LoginView.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+  Stack,
+  Text,
+  TextField,
+  FontWeights,
+  Button
+} from "office-ui-fabric-react";
+
+const boldStyle = { root: { fontWeight: FontWeights.semibold } };
+export default module.exports;
+
+export const LoginView: React.FunctionComponent = () => {
+  return (
+    <Stack
+      horizontalAlign="center"
+      verticalAlign="center"
+      verticalFill
+      styles={{
+        root: {
+          width: "960px",
+          margin: "0 auto",
+          textAlign: "center",
+          color: "#605e5c"
+        }
+      }}
+      gap={15}
+    >
+      {/* Insert Code Here */}
+    </Stack>
+  );
+};
