fbae3af2edab56e21867a33bddd8c6c106d987e7
Matthew Henry <henrym2@tcd.ie>
2020-02-14 22:23:08 +0000

    Added some examples to the app.js file

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,36 @@
 const express = require('express')
+
+//Load in dotenv for parsing environment variables (secrets and stuff)
+const dotenv = require('dotenv')
+
+const envLoaded = dotenv.config()
+
+if(envLoaded.error) {
+    throw envLoaded.error
+}
+
+//Load in body parser for parsing json
+const bodyParser = require("body-parser")
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//Make sure the express server uses body parser
+app.use(bodyParser.json())
+const port = process.env.APP_PORT;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+app.get('/', (req, res) => {
+    //Example of how you might construct a JSON response
+    let exampleObject = {
+        test: "test"
+    }
+    res.send(exampleObject)
+})
+
+app.listen(port, () => {
+    console.debug(
+        `Server launched on port ${port}\n`,
+        envLoaded.parsed
+    )
+    }
+)
