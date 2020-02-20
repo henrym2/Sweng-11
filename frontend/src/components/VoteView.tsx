d320832e500505dd0a882240244f8133c2c9a4a3
Matthew <howardm1@tcd.ie>
2020-02-20 11:21:51 +0000

    Added text and submit button to vote view

--- frontend/src/components/VoteView.tsx
+++ frontend/src/components/VoteView.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import {
   Stack,
   Text,
-  TextField,
   FontWeights,
-  Button,
+  PrimaryButton,
   Label,
   Slider,
-  IStackItemStyles
+  IStackItemStyles,
+  textAreaProperties
 } from "office-ui-fabric-react";
 
 const boldStyle = { root: { fontWeight: FontWeights.semibold } };
@@ -25,72 +25,86 @@ export const VoteView: React.FunctionComponent = () => {
       verticalFill
       styles={{
         root: {
-          width: "960px",
+          width: "100%",
           margin: "0 auto",
           textAlign: "center",
           color: "#605e5c"
         }
       }}
-      gap={15}
+      gap={50}
     >
-      <Stack 
-        horizontal 
-        horizontalAlign='space-between'
-        styles={{
-          root:{
-            width:'100%'
-          }
-        }}
-      >
-        <Stack.Item align='stretch' styles={{root:{width:'15%'}}}>
-          <Stack>
-            <Label styles={labelStyle}>Too Cold</Label>
-            <Label styles={labelStyle}>|</Label>
-          </Stack>
-        </Stack.Item>
-        <Stack.Item align='stretch' styles={{root:{width:'15%'}}}>
-          <Stack>
-            <Label styles={labelStyle}>A Bit Cold</Label>
-            <Label styles={labelStyle}>|</Label>
-          </Stack>
-        </Stack.Item>
-        <Stack.Item align='stretch' styles={{root:{width:'15%'}}}>
-          <Stack>
-            <Label styles={labelStyle}>Just Right</Label>
-            <Label styles={labelStyle}>|</Label>
-          </Stack>
-        </Stack.Item>
-        <Stack.Item align='stretch' styles={{root:{width:'15%'}}}>
-          <Stack>
-            <Label styles={labelStyle}>A Bit Hot</Label>
-            <Label styles={labelStyle}>|</Label>
-          </Stack>
-        </Stack.Item>
-        <Stack.Item align='stretch' styles={{root:{width:'15%'}}}>
-          <Stack>
-            <Label styles={labelStyle}>Too Hot</Label>
-            <Label styles={labelStyle}>|</Label>
-          </Stack>
-        </Stack.Item>
-
+      <Stack>
+        <Text variant="xxLarge">How is the current temperature?</Text>
       </Stack>
       <Stack
+        horizontalAlign="center"
         styles={{
           root:{
-            width:'86.75%'
+            width:"100%"
           }
         }}
       >
-          <Slider 
-            min={-2} 
-            max={2} 
-            defaultValue={0} 
-            originFromZero 
-            showValue={false} 
-            valueFormat={(value : number) => values[value + 2]}
-          />
-      </Stack>
+        <Stack 
+          horizontal 
+          horizontalAlign='space-between'
+          styles={{
+            root:{
+              width:'100%'
+            }
+          }}
+        >
+          <Stack.Item align='stretch' styles={{root:{width:'15%'}}}>
+            <Stack>
+              <Label styles={labelStyle}>Too Cold</Label>
+              <Label styles={labelStyle}>|</Label>
+            </Stack>
+          </Stack.Item>
+          <Stack.Item align='stretch' styles={{root:{width:'15%'}}}>
+            <Stack>
+              <Label styles={labelStyle}>A Bit Cold</Label>
+              <Label styles={labelStyle}>|</Label>
+            </Stack>
+          </Stack.Item>
+          <Stack.Item align='stretch' styles={{root:{width:'15%'}}}>
+            <Stack>
+              <Label styles={labelStyle}>Just Right</Label>
+              <Label styles={labelStyle}>|</Label>
+            </Stack>
+          </Stack.Item>
+          <Stack.Item align='stretch' styles={{root:{width:'15%'}}}>
+            <Stack>
+              <Label styles={labelStyle}>A Bit Hot</Label>
+              <Label styles={labelStyle}>|</Label>
+            </Stack>
+          </Stack.Item>
+          <Stack.Item align='stretch' styles={{root:{width:'15%'}}}>
+            <Stack>
+              <Label styles={labelStyle}>Too Hot</Label>
+              <Label styles={labelStyle}>|</Label>
+            </Stack>
+          </Stack.Item>
 
+        </Stack>
+        <Stack
+          styles={{
+            root:{
+              width:'88%'
+            }
+          }}
+        >
+            <Slider 
+              min={-2} 
+              max={2} 
+              defaultValue={0} 
+              originFromZero 
+              showValue={false} 
+              valueFormat={(value : number) => values[value + 2]}
+            />
+        </Stack>
+      </Stack>
+      <Stack>
+          <PrimaryButton>Submit</PrimaryButton>
+      </Stack>
     </Stack>
   );
 };
