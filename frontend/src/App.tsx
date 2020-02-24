f03884e7652ad5a3a1ee4bcd974f91a5613cd930
johnnywhite612 <johnnywhite612@gmail.com>
2020-02-24 10:31:59 +0000

    Setup react router and a new route and component for the admin page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { Stack, Text, Link, FontWeights } from "office-ui-fabric-react";
 import { UserSubmissionPage } from "./components/UserSubmissionPage";
+import { AdminPage } from "./components/AdminPage";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const boldStyle = { root: { fontWeight: FontWeights.semibold } };
 
 export const App: React.FunctionComponent = () => {
-  return <UserSubmissionPage />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <UserSubmissionPage />
+        </Route>
+        <Route path="/admin">
+          <AdminPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
