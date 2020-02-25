ca0ce04a9f910f6580ad04e0bff6a5d70cd54b4a
johnnywhite612 <johnnywhite612@gmail.com>
2020-02-25 11:46:30 +0000

    Frontend deployed to Azure cloud storage.

---
+++ frontend/build/precache-manifest.fb0194c256dc2d710f30adbd2d64c640.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43963e7a945393047c34670cdb749be3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "314ec9f7b6c4103e9924",
+    "url": "/static/css/main.bc8833f8.chunk.css"
+  },
+  {
+    "revision": "e6c94ba5a13b0faca79a",
+    "url": "/static/js/2.48cc1f89.chunk.js"
+  },
+  {
+    "revision": "314ec9f7b6c4103e9924",
+    "url": "/static/js/main.6cd82c53.chunk.js"
+  },
+  {
+    "revision": "fe5c7aae141f03b42984",
+    "url": "/static/js/runtime-main.8eea71de.js"
+  },
+  {
+    "revision": "c5dea73d33c45afd97dba19e26255956",
+    "url": "/static/media/logo.c5dea73d.svg"
+  },
+  {
+    "revision": "0f7d33961f4e3613f237ed8f114c82cf",
+    "url": "/static/media/mlogo.0f7d3396.png"
+  }
+]);
