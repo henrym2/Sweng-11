b2177157f6d5c362d1f55c47dd381db647e9b3a3
johnnywhite612 <johnnywhite612@gmail.com>
2020-03-13 17:02:10 +0000

    Admin dashboard now shows notifications in a list (not real data). You can also switch between views using the sidebar.

--- frontend/src/components/AdminNotification.tsx
+++ frontend/src/components/AdminNotification.tsx
@@ -1,89 +1,101 @@
 import React, { Component } from "react";
 import "./AdminPage.css";
 import { Depths } from "@uifabric/fluent-theme/lib/fluent/FluentDepths";
-import { Card, ICardTokens, ICardStyles, ICardSectionStyles } from "@uifabric/react-cards";
-import {Text, ITextStyles, FontWeights, Link, ILinkStyles, IconButton, Stack, IIconProps, IIconStyles} from "office-ui-fabric-react";
+import {
+  Card,
+  ICardTokens,
+  ICardStyles,
+  ICardSectionStyles
+} from "@uifabric/react-cards";
+import {
+  Text,
+  ITextStyles,
+  FontWeights,
+  Link,
+  ILinkStyles,
+  IconButton,
+  Stack,
+  IIconProps,
+  IIconStyles
+} from "office-ui-fabric-react";
 
 export type NotificationProps = {
-    title : string;
-    desctription : string;
+  title: string;
+  desctription: string;
 };
 
-export class AdminNotification extends Component<NotificationProps>{
+export default class AdminNotification extends Component<NotificationProps> {
+  render() {
+    const cardStyles: ICardStyles = {
+      root: {
+        width: "100%",
+        maxWidth: "100%",
+        minWidth: "400px",
+        minHeight: "120px",
+        marginBottom: "25px"
+      }
+    };
 
-    render(){
+    const cardTokens: ICardTokens = {};
 
-        const cardStyles : ICardStyles = {
-            root:{
-                width:'100%',
-                maxWidth: '1000px',
-                minWidth: '400px',
-                minHeight:'120px',
-                margin:'10px 10px'
-            }
-        }
-        
-        const cardTokens : ICardTokens = {
-        }
+    const titleStyles: ITextStyles = {
+      root: {
+        fontSize: 22,
+        fontWeight: FontWeights.bold
+      }
+    };
 
-        const titleStyles : ITextStyles = {
-            root:{
-                fontSize:20,
-                fontWeight:FontWeights.bold,
-            }
-        }
+    const descriptionStyles: ITextStyles = {
+      root: {
+        fontSize: 18,
+        marginTop: 2
+      }
+    };
 
-        const descriptionStyles : ITextStyles = {
-            root : {
-                fontSize: 15,
-                marginTop:2
-            }
-        }
+    const linkStyles: ILinkStyles = {
+      root: {
+        textDecoration: "underline",
+        fontSize: 16,
+        marginTop: 2,
+        color: "#E85B6D"
+      }
+    };
 
-        const linkStyles : ILinkStyles = {
-            root : {
-                textDecoration:'underline',
-                fontSize: 15,
-                marginTop:2
-            }
-        }
-        
-        const cardSectionStyles : ICardSectionStyles = {
-            root : {
-                width : '95%',
-                margin : '10px 15px'
-            }
-        }
+    const cardSectionStyles: ICardSectionStyles = {
+      root: {
+        width: "95%",
+        margin: "10px 15px"
+      }
+    };
 
-        const buttonSectionStyles : ICardSectionStyles = {
-            root : {
-                alignSelf: 'stretch',
-                margin : 10,
-            }
-        }
+    const buttonSectionStyles: ICardSectionStyles = {
+      root: {
+        alignSelf: "stretch",
+        margin: 10
+      }
+    };
 
-        const buttonProps : IIconProps = {
-            iconName : 'Cancel',
-        }
+    const buttonProps: IIconProps = {
+      iconName: "Cancel"
+    };
 
-        const buttonStyles : IIconStyles = {
-            root : {
-                color : 'grey'
-            }
-        }
+    const buttonStyles: IIconStyles = {
+      root: {
+        color: "grey"
+      }
+    };
 
-        return (
-        <Card tokens={cardTokens} horizontal styles={cardStyles}>
-            <Card.Section styles={cardSectionStyles} fill>
-                <Text styles={titleStyles}>{this.props.title}</Text>
-                <Text styles={descriptionStyles}>{this.props.desctription}</Text>
-                <Link styles={linkStyles}>More details</Link>
-            </Card.Section>
-            <Card.Section styles={buttonSectionStyles}>
-                <IconButton iconProps={buttonProps} styles={buttonStyles}/>
-            </Card.Section>
-        </Card>
-        );
-    }
-
+    return (
+      <Card tokens={cardTokens} horizontal styles={cardStyles}>
+        <Card.Section styles={cardSectionStyles} fill>
+          <Text styles={titleStyles}>{this.props.title}</Text>
+          <Text styles={descriptionStyles}>{this.props.desctription}</Text>
+          <Link styles={linkStyles}>More details</Link>
+        </Card.Section>
+        <Card.Section styles={buttonSectionStyles}>
+          <IconButton iconProps={buttonProps} styles={buttonStyles} />
+        </Card.Section>
+      </Card>
+    );
+  }
 }
--- frontend/src/components/AdminPage.tsx
+++ frontend/src/components/AdminPage.tsx
@@ -4,9 +4,10 @@ import { Depths } from "@uifabric/fluent-theme/lib/fluent/FluentDepths";
 import { ICardTokens } from "@uifabric/react-cards";
 import mlogo from "../images/mlogo.png";
 import thermaLogo from "../images/logo-white.svg";
-import LoginView from "./LoginView";
-import VoteView from "./VoteView";
-import VoteConfirmationView from "./VoteConfirmationView";
+import AdminNotification from "./AdminNotification";
+import SideBarButton from "./SideBarButton";
+import alertIcon from "../images/alert-icon.svg";
+import mapIcon from "../images/map-icon.svg";
 
 type MyProps = {};
 type MyState = {
@@ -18,6 +19,53 @@ export class AdminPage extends Component<MyProps, MyState> {
 
   state: MyState = {
     pageState: 0
+  };
+
+  viewAlerts = () => {
+    this.setState({ pageState: 0 });
+  };
+
+  viewFloorplan = () => {
+    this.setState({ pageState: 1 });
+  };
+
+  alertScreen = () => {
+    return (
+      <div className="admin-page__notifications">
+        <AdminNotification
+          title="Please Adjust Temperature"
+          desctription="A temperature adjustment is needed on floor 4."
+        />
+        <AdminNotification
+          title="Please Adjust Temperature"
+          desctription="A temperature adjustment is needed on floor 2."
+        />
+        <AdminNotification
+          title="Please Adjust Temperature"
+          desctription="A temperature adjustment is needed on floor 2."
+        />
+        <AdminNotification
+          title="Please Adjust Temperature"
+          desctription="A temperature adjustment is needed on floor 2."
+        />
+        <AdminNotification
+          title="Please Adjust Temperature"
+          desctription="A temperature adjustment is needed on floor 2."
+        />
+        <AdminNotification
+          title="Please Adjust Temperature"
+          desctription="A temperature adjustment is needed on floor 2."
+        />
+        <AdminNotification
+          title="Please Adjust Temperature"
+          desctription="A temperature adjustment is needed on floor 2."
+        />
+      </div>
+    );
+  };
+
+  floorPlanScreen = () => {
+    return <div className="admin-page__floor-plan"></div>;
   };
 
   render() {
@@ -32,12 +80,23 @@ export class AdminPage extends Component<MyProps, MyState> {
           Top Bar (Milu)
         </div>
         <div className="admin-page__bottom">
-          <div className="admin-page__side-bar">Side Bar</div>
-          <div className="admin-page__notifications">
-            Page Content (Matthew - put notification/alert card in here for now)
-            <div>asdfasdf</div>
-            <div>asdf8888</div>
+          <div className="admin-page__side-bar">
+            <SideBarButton
+              title="Alerts &amp; Notifications"
+              icon={alertIcon}
+              color={this.state.pageState == 0 ? "#F8F8FF" : "white"}
+              switchDisplay={() => this.viewAlerts()}
+            />
+            <SideBarButton
+              title="Floorplan &amp; Heatmap"
+              icon={mapIcon}
+              color={this.state.pageState == 1 ? "#F8F8FF" : "white"}
+              switchDisplay={() => this.viewFloorplan()}
+            />
           </div>
+          {this.state.pageState == 0
+            ? this.alertScreen()
+            : this.floorPlanScreen()}
         </div>
       </div>
     );
