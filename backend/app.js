84177e83d9c109d4cc2a5b744ba30738d88c2c22
Matthew Henry <henrym2@tcd.ie>
2020-03-27 13:47:56 +0000

    Testing without CORs whitelist

--- backend/app.js
+++ backend/app.js
@@ -29,18 +29,18 @@ app.use(bodyParser.json());
 
 //Setup CORS
 var cors = require("cors");
-var whitelist = ["http://localhost:3000", "http://frontendurl"];
-var corsOptions = {
-  origin: function(origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
-};
+// var whitelist = ["http://localhost:3000", "http://frontendurl"];
+// var corsOptions = {
+//   origin: function(origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   }
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 //let votes = new Array();
 
